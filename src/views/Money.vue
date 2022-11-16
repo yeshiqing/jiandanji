c41ea00aac652b326b810f865058da8393149a53
jeffrey <jeffreyye@qq.com>
2022-11-16 20:45:47 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,57 @@
 <template>
-  <div>记账
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+      <p>记账</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,5 +63,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
