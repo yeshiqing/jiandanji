30384c8bda8859876b75bd4c9693a493d770dbb6
jeffrey <jeffreyye@qq.com>
2022-11-23 13:30:00 +0800

    解决 SVG 自带 fill 属性的问题 ......

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,14 @@ module.exports = {
     const dir = path.resolve(__dirname, 'src/assets/icons')
 
     config.module
-      .rule('svg-sprite')
-      .test(/\.svg$/)
-      .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+    .rule('svg-sprite')
+    .test(/\.svg$/)
+    .include.add(dir).end() // 包含 icons 目录
+    .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+    .use('svgo-loader').loader('svgo-loader').tap(options => ({
+      ...options,
+      plugins: [{removeAttrs: {attrs: 'fill'}}]
+    })).end()
 
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg 排除 icons 目录
